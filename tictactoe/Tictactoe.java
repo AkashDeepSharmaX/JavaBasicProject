45782494896cdf45339895bfafe2f4d09d420940
AkashDeepSharmaX <akashdeepsharma665@gmail.com>
2024-12-19 09:26:48 +0530

    working on Tic Tac Toe game

---
+++ tictactoe/Tictactoe.java
@@ -0,0 +1,16 @@
+
+import java.util.Scanner;
+
+public class Tictactoe {
+               Scanner sc = new Scanner(System.in);
+               public char getInput(String msg) {
+                              System.out.println(msg);
+                              return sc.next().charAt(0); }
+               public static void main(String[] args) {
+                              Tictactoe tictactoe=new Tictactoe();
+                char[][] array = new char[3][3];
+
+
+                
+               }
+}

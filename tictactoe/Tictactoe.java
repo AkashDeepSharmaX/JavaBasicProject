57149d8ba85402e2a26e20564b7df7a19a2fb321
AkashDeepSharmaX <akashdeepsharma665@gmail.com>
2024-12-22 22:04:14 +0530

    intial commit

--- tictactoe/Tictactoe.java
+++ tictactoe/Tictactoe.java
@@ -9,6 +9,14 @@ public class Tictactoe {
                public static void main(String[] args) {
                               Tictactoe tictactoe=new Tictactoe();
                 char[][] array = new char[3][3];
+             int row=   tictactoe.getInput("enter the row ");
+
+               int column=  tictactoe.getInput("enter the column");
+                if (row>3 && column>3){
+                    System.out.println("value can't be greater ");
+                }
+
+            
 
 
                 

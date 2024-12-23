f94648e4c944c628ad47be0a3672e2f66cd48e6f
AkashDeepSharmaX <akashdeepsharma665@gmail.com>
2024-12-23 21:53:27 +0530

    still bugs

--- tictactoe/Tictactoe.java
+++ tictactoe/Tictactoe.java
@@ -1,24 +1,53 @@
-
 import java.util.Scanner;
 
 public class Tictactoe {
-               Scanner sc = new Scanner(System.in);
-               public char getInput(String msg) {
-                              System.out.println(msg);
-                              return sc.next().charAt(0); }
-               public static void main(String[] args) {
-                              Tictactoe tictactoe=new Tictactoe();
-                char[][] array = new char[3][3];
-             int row=   tictactoe.getInput("enter the row ");
+    public static void main(String[] args) {
+        char[][] board = {
+            {'-', '-', '-'},
+            {'-', '-', '-'},
+            {'-', '-', '-'}
+        };
 
-               int column=  tictactoe.getInput("enter the column");
-                if (row>3 && column>3){
-                    System.out.println("value can't be greater ");
-                }
+        Scanner sc = new Scanner(System.in);
+        boolean gameRunning = true;
 
-            
+        System.out.println("Welcome to Tic Tac Toe!");
+        while (gameRunning) {
+        
+            printBoard(board);
 
+            System.out.println("Enter the row (0-2), column (0-2), and character (X or O) you want to place:");
+            int row = sc.nextInt();
+            int col = sc.nextInt();
+            char symbol = sc.next().charAt(0);
 
-                
-               }
+           if (isValidMove(board, row, col)) {
+                board[row][col] = symbol;
+            } else {
+                System.out.println("Invalid move. Try again.");
+            }
+
+            // Ask if the user wants to continue
+            System.out.println("Do you want to continue? (yes or no)");
+            String continueGame = sc.next();
+            if (continueGame.equalsIgnoreCase("no")) {
+                gameRunning = false;
+            }
+        }
+
+        System.out.println("Final board:");
+        printBoard(board);
+        sc.close();
+    }
+    public static void printBoard(char[][] board) {
+        for (char[] row : board) {
+            for (char cell : row) {
+                System.out.print(cell + " ");
+            }
+            System.out.println();
+        }
+    }
+    public static boolean isValidMove(char[][] board, int row, int col) {
+        return row >= 0 && row < 3 && col >= 0 && col < 3 && board[row][col] == '-';
+    }
 }

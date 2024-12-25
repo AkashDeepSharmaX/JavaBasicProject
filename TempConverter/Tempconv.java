3e1a05e3d10e0f6a15f8610d0b8ee4e8a38ed3fe
AkashDeepSharmaX <akashdeepsharma665@gmail.com>
2024-12-25 21:21:45 +0530

    intiate temprature converter

---
+++ TempConverter/Tempconv.java
@@ -0,0 +1,7 @@
+import java.util.*;
+public class Tempconv{
+               public static void main(String[] args) {
+                              Scanner sc = new Scanner(System.in);
+
+               }
+}

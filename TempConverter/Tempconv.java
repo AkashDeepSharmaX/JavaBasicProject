ba1ff0642372e96975f1a411eff184e9601c6c1e
AkashDeepSharmaX <akashdeepsharma665@gmail.com>
2024-12-25 21:37:56 +0530

    add a neew method

--- TempConverter/Tempconv.java
+++ TempConverter/Tempconv.java
@@ -1,7 +1,13 @@
-import java.util.*;
-public class Tempconv{
-               public static void main(String[] args) {
-                              Scanner sc = new Scanner(System.in);
 
-               }
+import java.util.*;
+
+public class Tempconv {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Welcome to the Temperature Converter!\n 1. Celsius to Fahrenheit\n 2. Celsius to Kelvin\n 3. Fahrenheit to Celsius");
+        System.out.println("4. Fahrenheit to Kelvin/5. Kelvin to Celsius\n6. Kelvin to Fahrenheit\nChoose an option (1-6):");
+        int userInput  = sc.nextInt();
+
+    }
 }
